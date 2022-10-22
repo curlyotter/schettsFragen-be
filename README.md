cb923c1370f7b2481a11ec463c21209cb2da2898
Yannick Peter <yannick.peter@fino.digital>
2022-10-22 19:09:21 +0200

    implement config file handling and pr creation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # schettsFragen-be
+best backend for the best app
+
+# TODOs
+- [ ] add questions to yaml
+- [ ] implement git functionallity
+    - [ ] add yaml file
+    - [ ] create commit
+    - [ ] push to branch
+    - [ ] auth with github account (oauth app)
+    - [x] create pull request
---
+++ pkg/writer/writer.go
@@ -0,0 +1,9 @@
+package writer
+
+import "github.com/curlyotter/schettsFragen-be/pkg/question"
+
+// TODO implement writer that adds questions to yaml (questions/questions.yaml)
+// QuestionsToYAML recieves a slice of questions and adds them to the questions yaml
+func QuestionsToYAML(questions []question.Question) error {
+	return nil
+}
